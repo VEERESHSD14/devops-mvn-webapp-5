6ab32fb06a3968def972fd768a6355a4a6769b05
Pavithran V <pavithranv2195@gmail.com>
2024-11-11 16:42:28 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
-# Update and install Docker
 sudo yum update -y
+sudo yum search docker
+sudo yum info docker
 sudo yum install -y docker
-
-# Enable and start Docker service
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+sudo systemctl status docker.service
 docker --version
-
-# Install Git (optional, if required)
-sudo yum install -y git
-
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo yum install git -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
